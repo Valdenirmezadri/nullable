6bcdc52ba613013ecfcf791f4fffa89e854c973e
Valdenir Luíz Mezadri Junior <juniordarkeden@gmail.com>
2022-06-24 19:33:27 -0300

    add string null

--- json_test.go
+++ json_test.go
@@ -23,6 +23,13 @@ func marshalUnmarshalJSON(t *testing.T, target interface{}) {
 			return
 		}
 		tests.AssertEqual(t, unserialized, target)
+	case nullable.String:
+		var unserialized nullable.String
+		if err := json.Unmarshal(serialized, &unserialized); err != nil {
+			t.Fatalf("Failed to unmarshal %T because: %s", target, err)
+			return
+		}
+		tests.AssertEqual(t, unserialized, target)
 	default:
 		t.Fatalf("%T is not registered at json_test.go", target)
 	}
